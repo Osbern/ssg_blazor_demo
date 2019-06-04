72e6c7f27ef7d25e8f06fac90b0513c3c0c148b2
DESTOUCHES Thibaud <thibaud.destouches@soprasteria.com>
2019-06-04 13:10:05 +0200

    Init repo

---
+++ Model/CommentModel.cs
@@ -0,0 +1,15 @@
+namespace ssg_blazor_demo
+{
+    public class CommentModel
+    {
+        public int PostId { get; set; }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Body { get; set; }
+    }
+}
---
+++ Model/PostModel.cs
@@ -0,0 +1,13 @@
+namespace ssg_blazor_demo
+{
+    public class PostModel
+    {
+        public int UserId { get; set; }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+    }
+}
